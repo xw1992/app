af6de0f6437cf1ed1ba65711ee3be10ef0f0fb99
xw1992 <xw920423@gmail.com>
2014-10-27 20:29:01 -0400

    schema for user_info and modification for user_trips
    schema for user_info and modification for user_trips

---
+++ database/migrations/2014_10_27_195541_create_user_infos_add_attribt_to_user_trips.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserInfosAddAttribtToUserTrips extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('user_infos', function($table){
+			$table->integer('user_id')->unsigned();
+			$table->primary('user_id');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->string('hometown_state');
+			$table->string('passport_no');
+			$table->string('major_academic_interest');
+			$table->string('dietary_allergies_access_needs')->nullable();
+			$table->string('foreign_languages')->nullable();
+			$table->boolean('smoke');
+			$table->text('allergy_medical_conditions')->nullable();
+			$table->text('relevant_experience_interest')->nullable();
+			$table->text('bio')->nullable();
+		});
+		
+		Schema::table('user_trips', function($table){
+			$table->boolean('trip_leader')->nullable();
+		
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('user_infos');
+		
+		Schema::table('user_trips', function($table){
+			$table->dropColumn('trip_leader');
+		});
+		
+	}
+
+}

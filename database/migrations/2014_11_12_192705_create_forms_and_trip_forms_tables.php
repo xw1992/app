a0d80f027cc6f4df590117f6a871d84434072ef4
xw1992 <xw920423@gmail.com>
2014-11-12 14:56:04 -0500

    schema for forms and trip_forms

---
+++ database/migrations/2014_11_12_192705_create_forms_and_trip_forms_tables.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormsAndTripFormsTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('forms', function($table){
+			$table->increments('id');
+			$table->string('name');
+			$table->string('location');
+			$table->timestamps();
+		});
+		
+		Schema::create('trip_forms', function($table){
+			$table->increments('id');
+			$table->integer('trip_id')->unsigned();
+			$table->integer('form_id')->unsigned();
+			$table->foreign('trip_id')->references('id')->on('trips')->onDelete('cascade');
+			$table->foreign('form_id')->references('id')->on('forms')->onDelete('cascade');
+			$table->timestamps();
+		
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('trip_forms');
+		Schema::drop('forms');
+	}
+
+}

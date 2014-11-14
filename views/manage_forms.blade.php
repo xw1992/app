a3ed99933559a693145c9d4295a4b1ded0e8ecef
Anna <kanean01@gettysburg.edu>
2014-11-14 13:30:32 -0500

    manage_forms update

--- views/manage_forms.blade.php
+++ views/manage_forms.blade.php
@@ -14,7 +14,7 @@
 		<tr>
 			<th>Form Name</th>
 			<th>Trips</th>
-			<th>Manage</th>
+			<th>Delete</th>
 		</tr>
 	</thead>
 	<tr>
@@ -23,8 +23,8 @@
 			Trips
 		</button>
 	</td>
-	<td><button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#formModal$user->id}}">
-		Manage
+	<td><button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#deleteFormModal">
+		Delete
 	</button>
 </td>
 </tr>
@@ -77,8 +77,10 @@
 					<div class="row">
 						<h5>Which trips would you like to add this form to?</h5>
 						<form>
-							trip1
-							{{Form::checkbox('tripCheck', 0)}}
+							@foreach{{$trips as $trip}}
+							{{$trip->name}}
+							{{Form::checkbox('formTrip')}}
+							@endforeach
 						</form>
 
 					</div>
@@ -93,40 +95,33 @@
 	</div>
 </div>
 
-
-
-
-
-
-</table>
-<center>
-	<button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#newFormModal">
-		Add a Form
-	</button>
-</center>
-<div class="modal fade" id="newFormModal" tabindex="-1" role="dialog" aria-labelledby="newFormModalLabel" aria-hidden="true">
+<div class="modal fade" id="deleteFormModal" tabindex="-1" role="dialog" aria-labelledby="deleteFormModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-				<h4 class="modal-title" id="newFormModalLabel">Add a new form</h4>
+				<h4 class="modal-title" id="deleteFormModalLabel">Delete Form</h4>
 			</div>
 			<div class="modal-body">
+				<h5>{{$form->name}}</h5>
 				<form>
-					<h5>Name:</h5>
-					{{Form::text('form_name')}}<br><br>
-					<h5>Select a file from your computer:</h5>
-					{{Form::file('image')}}
-
+					<h5>Are you sure you want to delete this form?</h5>
+					<button type="submit" class="btn btn-default">Yes, delete</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
 				</form>
 
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-				<button type="submit" class="btn btn-info">Save changes</button>
 			</div>
 		</div>
 	</div>
 </div>
+
+
+</table>
+<center>
+	<button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#deleteFormModal">
+		Add Form
+	</button>
+</center>
+
 
 @stop

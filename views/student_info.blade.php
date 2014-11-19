ec70fb67b70d63b2ac2e3ef3a5422548838b87e6
Anna <kanean01@gettysburg.edu>
2014-11-18 20:48:07 -0500

    student_info

--- views/student_info.blade.php
+++ views/student_info.blade.php
@@ -3,24 +3,24 @@
 @stop
 @section('content')    
 <h4>Student Information: $user->fname . $user->lname}}</h4><br><br>
-
+{{Form::open(['url' => ''])}}
 <div class="row">
 	<div class="col-xs-4 col-xs-4 col-xs-4">
 		<div class="form-group">
 			<h5>First Name:</h5>
-			{{Form::text('fname','$user-fname')}}
+			{{Form::text('fname', $user-fname)}}
 		</div>
 	</div>
 	<div class="col-xs-4 col-xs-4 col-xs-4">
 		<div class="form-group">
 			<h5>Middle Name:</h5>
-			{{Form::text('mname','$user-mname')}}
+			{{Form::text('mname', $user-mname)}}
 		</div>
 	</div>
 	<div class="col-xs-4 col-xs-4 col-xs-4">
 		<div class="form-group">
 			<h5>Last Name:</h5>
-			{{Form::text('lname','$user-lname')}}
+			{{Form::text('lname', $user-lname)}}
 		</div>
 	</div>
 </div>
@@ -28,13 +28,13 @@
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 		<h5>Date of Birth:</h5>
-		{{Form::text('dob','$user->dob')}}
+		{{Form::text('dob', $user->dob)}}
 	</div>
 </div>
 <div class="col-sm-4 col-xs-4 col-sm-4">
 	<div class="form-group">
 		<h5>Gender:</h5>
-		{{Form::text('gender','$user->gender')}}
+		{{Form::text('gender', $user->gender)}}
 	</div>
 </div>
 </div>
@@ -42,13 +42,13 @@
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Country:</h5>
-			{{Form::text('country','$user->country')}}
+			{{Form::text('country', $user->country)}}
 		</div>
 	</div>
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Passport Number:</h5>
-			{{Form::text('passport_no','$user->passport_no')}}
+			{{Form::text('passport_no', $user->passport_no)}}
 		</div>
 	</div>
 </div>
@@ -56,13 +56,13 @@
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Address:</h5>
-			{{Form::text('address','$user->address')}}
+			{{Form::text('address', $user->address)}}
 		</div>
 	</div>
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Phone Number:</h5>
-			{{Form::text('phone_no','$user->phone_no')}}
+			{{Form::text('phone_no', $user->phone_no)}}
 		</div>
 	</div>
 </div>
@@ -70,19 +70,19 @@
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Emergency Contact:</h5>
-			{{Form::text('emergency_contact_name','$user->emergency_contact_name')}}
+			{{Form::text('emergency_contact_name', $user->emergency_contact_name)}}
 		</div>
 	</div>
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Emergency Contact Phone:</h5>
-			{{Form::text('emergency_contact_phone','$user->emergency_contact_phone')}}
+			{{Form::text('emergency_contact_phone', $user->emergency_contact_phone)}}
 		</div>
 	</div>
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Emergency Contact Address:</h5>
-			{{Form::text('emergency_contact_address','$user->emergency_contact_address')}}
+			{{Form::text('emergency_contact_address', $user->emergency_contact_address)}}
 		</div>
 	</div>
 </div>
@@ -90,19 +90,19 @@
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Student Id:</h5>
-			{{Form::text('student_id','$user->student_id')}}
+			{{Form::text('student_id', $user->student_id)}}
 		</div>
 	</div>
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Campus Box:</h5>
-			{{Form::text('campus_box','$user->campus_box')}}
+			{{Form::text('campus_box', $user->campus_box)}}
 		</div>
 	</div>
 	<div class="col-sm-4 col-xs-4 col-sm-4">
 		<div class="form-group">
 			<h5>Class Year:</h5>
-			{{Form::text('class_year','$user->class_year')}}
+			{{Form::text('class_year', $user->class_year)}}
 		</div>
 	</div>
 </div>

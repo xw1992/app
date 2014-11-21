c56194f8affe33beb62eecb52583a5fe86b7eeb6
xw1992 <xw920423@gmail.com>
2014-11-21 14:56:42 -0500

    user_forms schema

---
+++ database/migrations/2014_11_21_195305_create_user_forms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFormsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('user_forms',function($table){
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('form_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->foreign('form_id')->references('id')->on('forms')->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('user_forms');
+	}
+
+}
